ed584fb23966aa2f6de09620e581d27be8105ed2
cekip88 <cekip@mail.ru>
2020-10-18 16:25:49 +0600

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+  let str = n + '',
+      tempStr = '';
+  for (let i = str.length - 1; i >= 0; i--){
+      if(str[i] === '-') continue
+      tempStr += str[i];
+  }
+  return tempStr * 1;
+};
